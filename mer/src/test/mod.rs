ef202163933ea492b4aa83c1b4fc4a8450c04d3b
Paul Volavsek <paul.volavsek@gmail.com>
2021-01-19 14:37:46 +0100

    :white_check_mark:

--- mer/src/test/mod.rs
+++ mer/src/test/mod.rs
@@ -1,10 +1,11 @@
-use crate::interfaces::{Backend, Frontend};
+use crate::{Mer, MerInit, interfaces::{Backend, Frontend}};
 
 use crate::{Call, Reply};
 
 use anyhow::Result;
 
 struct MockBackend {}
+
 impl Backend for MockBackend {
   type Intermediate = String;
 
@@ -15,14 +16,15 @@ impl Backend for MockBackend {
     Ok(())
   }
 
-  fn receiver<T>(&mut self, receiver: T) -> Result<()>
+  fn receiver<T>(&mut self, _receiver: T) -> Result<()>
   where
-    T: Fn(Call<Self::Intermediate>) -> Result<Reply<Self::Intermediate>> + Send + Sync + 'static {
-      Ok(())
-    }
+    T: Fn(Call<Self::Intermediate>) -> Result<Reply<Self::Intermediate>> + Send + Sync + 'static,
+  {
+    Ok(())
+  }
 
   fn call(&mut self, call: Call<Self::Intermediate>) -> Result<Reply<Self::Intermediate>> {
-    Ok(Reply { payload: "".to_string() })
+    Ok(Reply { payload: call.payload })
   }
 
   fn serialize<T: serde::Serialize>(from: &T) -> Result<Self::Intermediate> {
@@ -31,9 +33,10 @@ impl Backend for MockBackend {
 
   fn deserialize<'b, T>(from: &'b Self::Intermediate) -> Result<T>
   where
-    T: for<'de> serde::Deserialize<'de> {
-      serde_json::from_str(from).map_err(|e| e.into())
-    }
+    T: for<'de> serde::Deserialize<'de>,
+  {
+    serde_json::from_str(from).map_err(|e| e.into())
+  }
 }
 
 struct MockFrontend {}
@@ -42,19 +45,54 @@ impl Frontend for MockFrontend {
   type Backend = MockBackend;
   type Intermediate = String;
 
-  fn caller<T>(&mut self, caller: T) -> Result<()>
+  fn caller<T>(&mut self, _caller: T) -> Result<()>
   where
-    T: Fn(Call<<Self::Backend as Backend>::Intermediate>) -> Result<Reply<<Self::Backend as Backend>::Intermediate>> + Send + Sync + 'static {
-      Ok(())
-    }
+    T: Fn(Call<<Self::Backend as Backend>::Intermediate>) -> Result<Reply<<Self::Backend as Backend>::Intermediate>> + Send + Sync + 'static,
+  {
+    Ok(())
+  }
 
   fn receive(&self, call: Call<<Self::Backend as Backend>::Intermediate>) -> Result<Reply<<Self::Backend as Backend>::Intermediate>> {
-    Ok(Reply { payload: "".to_string() })
+    Ok(Reply { payload: call.payload })
   }
+}
+
+impl MockFrontend {
+  fn call<T>(&self, call: T) -> T {
+    call
+  }
+}
+
+fn setup<'a>() -> Mer<'a, MockBackend, MockFrontend> {
+  MerInit {
+    backend: MockBackend {},
+    frontend: MockFrontend {},
+  }.init()
+}
+
+#[test]
+fn init() {
+  setup();
+}
+
+#[test]
+fn start() {
+  setup().start().unwrap()
 }
 
 
 #[test]
-fn init() {
+fn stop() {
+  setup().stop().unwrap()
+}
 
+#[test]
+fn frontend() {
+  let rnd: i32 = rand::random();
+  setup().frontend(|f| assert_eq!(f.call(rnd), rnd)).unwrap()
+}
+
+#[test]
+fn backend() {
+  setup().backend(|_b| ()).unwrap();
 }
