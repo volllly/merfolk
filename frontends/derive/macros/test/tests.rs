e7c52d4aef6e9a1c7c707d392b073757bdbfd5b2
Paul Volavsek <paul.volavsek@gmail.com>
2021-02-08 16:06:45 +0100

    :white_check_mark:

--- frontends/derive/macros/test/tests.rs
+++ frontends/derive/macros/test/tests.rs
@@ -1,67 +1,67 @@
 use mer_frontend_derive::*;
 
-// #[test]
-// fn test_generic() {
-//   #[frontend]
-//   struct Data<T>
-//   where
-//     T: std::ops::Add<Output = T> + for<'de> serde::Deserialize<'de> + serde::Serialize + Copy + Send,
-//   {
-//     pub offset: T,
-//   }
+#[test]
+fn test_generic() {
+  #[frontend]
+  struct Data<T>
+  where
+    T: std::ops::Add<Output = T> + for<'de> serde::Deserialize<'de> + serde::Serialize + Copy + Send,
+  {
+    pub offset: T,
+  }
 
-//   #[frontend(target = "Data")]
-//   trait Service<T>
-//   where
-//     T: std::ops::Add<Output = T> + for<'de> serde::Deserialize<'de> + serde::Serialize + Copy + Send,
-//   {
-//     fn add(a: T, b: T) -> T::Output {
-//       a + b
-//     }
+  #[frontend(target = "Data")]
+  trait Service<T>
+  where
+    T: std::ops::Add<Output = T> + for<'de> serde::Deserialize<'de> + serde::Serialize + Copy + Send,
+  {
+    fn add(a: T, b: T) -> T::Output {
+      a + b
+    }
 
-//     fn add_with_offset(&self, a: T, b: T) -> T::Output {
-//       a + b + self.offset
-//     }
-//   }
-// }
+    fn add_with_offset(&self, a: T, b: T) -> T::Output {
+      a + b + self.offset
+    }
+  }
+}
 
-// #[test]
-// fn test() {
-//   #[frontend]
-//   struct Data {}
+#[test]
+fn test() {
+  #[frontend]
+  struct Data {}
 
-//   #[frontend(target = "Data")]
-//   trait Service {
-//     fn add(a: i32, b: i32) -> i32 {
-//       a + b
-//     }
+  #[frontend(target = "Data")]
+  trait Service {
+    fn add(a: i32, b: i32) -> i32 {
+      a + b
+    }
 
-//     fn empty() {}
+    fn empty() {}
 
-//     fn itentity(a: i32) -> i32 {
-//       a
-//     }
-//   }
-// }
+    fn itentity(a: i32) -> i32 {
+      a
+    }
+  }
+}
 
-// #[test]
-// fn test_with_data() {
-//   #[frontend]
-//   struct Data {
-//     pub offset: i32,
-//   }
+#[test]
+fn test_with_data() {
+  #[frontend]
+  struct Data {
+    pub offset: i32,
+  }
 
-//   #[frontend(target = "Data")]
-//   trait Service {
-//     fn add(a: i32, b: i32) -> i32 {
-//       a + b
-//     }
+  #[frontend(target = "Data")]
+  trait Service {
+    fn add(a: i32, b: i32) -> i32 {
+      a + b
+    }
 
-//     fn add_with_offset(&self, a: i32, b: i32) -> i32 {
-//       a + b + self.offset
-//     }
-//   }
-// }
+    fn add_with_offset(&self, a: i32, b: i32) -> i32 {
+      a + b + self.offset
+    }
+  }
+}
 
 
 #[test]
